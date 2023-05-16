228b83f305d213f5b580dec1a05310c5e3b69ff9
Olena Nesteruk <pavlena81@gmail.com>
2023-05-17 00:49:47 +0300

    adds icons

--- app/component/sidebar.jsx
+++ app/component/sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
+import {Envelope, Linkedin} from  './icons/'
 
 
 const Sidebar = ({ data}) => {
-    const {name, role, education} = data;
+    const {name, role, education, contactLinks} = data;
     return (
         <div>sidebar
             <Image />
@@ -11,8 +12,13 @@ const Sidebar = ({ data}) => {
             <h2>{role}</h2>
             <p>{education}</p>
             <div>
-                <h3>Contact me</h3>
-                <a href=''></a>
+                <h3>CONTACT ME</h3>
+                <a href={contactLinks?.[0]} aria-label={'email link'}>
+                    <Envelope/>
+                </a>
+                <a href={contactLinks?.[2]} aria-label={'linkedin link'}>
+                    <Linkedin/>
+                </a>
             </div>
         </div>
     )
