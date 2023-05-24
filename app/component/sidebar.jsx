1a5c2c9be82b77a919cfb94b90db74a76c96de7d
Olena Nesteruk <pavlena81@gmail.com>
2023-05-25 01:49:28 +0300

    update sidebar

--- app/component/sidebar.jsx
+++ app/component/sidebar.jsx
@@ -6,8 +6,8 @@ import {Envelope, Linkedin} from  './icons/'
 const Sidebar = ({ data}) => {
     const {name, role, education, contactLinks} = data;
     return (
-        <div className='bg-black flex flex-col h-screen w-full content-between sm:justify-around sm:w-1/3 sm:fixed'>sidebar
-            <div className='text-white flex flex-col p-10 items-senter'>
+        <div className='bg-black flex flex-col h-screen w-full content-between sm:justify-around sm:w-1/3 sm:fixed'>
+            <div className='text-white text-center flex flex-col p-10 items-center'>
                 <Image
                     priority
 					width={300}
@@ -21,18 +21,23 @@ const Sidebar = ({ data}) => {
                 <p className='mb-8'>{education[0]}</p>
                 <p className='mb-2'>{education[1]}</p>
             
-            <div>
-                <h3>CONTACT ME</h3>
-                
-                <a href={contactLinks?.[0]} aria-label={'email link'}>
+            <div className='text-white text-center mb-4 mt-4 sm:mt-8'>
+                <h3 className='mb-2'>CONTACT ME</h3>
+                <div className='flex flex-row justify-center gap-2'>
+                        <a
+                            className='icons-contactme'
+                            href={contactLinks?.[0]} aria-label={'email link'}>
                     <Envelope/>
                 </a>
-                <a href={contactLinks?.[2]} aria-label={'linkedin link'}>
+                        <a
+                            className='icons-contactme'
+                            href={contactLinks?.[2]} aria-label={'linkedin link'}>
                     <Linkedin/>
                 </a>
                 {/* <a href={contactLinks?.[3]} aria-label={'github link'}>
                     <Github/>
                 </a> */}
+                </div>
             </div></div>
         </div>
     )
